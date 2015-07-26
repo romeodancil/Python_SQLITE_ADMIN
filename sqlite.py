125af35fe85110ff2db773bdd065a44204f0529a
= <romeodancil30@gmail.com>
2015-07-26 11:05:43 +0800

    convert to oop

--- sqlite.py
+++ sqlite.py
@@ -1,76 +1,76 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sqlite3 as liteDb
 import sys
 import platform
 import os
-
 from prettytable import PrettyTable # pip install PrettyTable
 
-# start checking database
-if (len(sys.argv) > 1):
-	con = liteDb.connect(sys.argv[1])
-else:
-	con = liteDb.connect("example.db")
+class SqliteAdmin:
+	con = ''
+	cur = ''
 
-cur = ''
-
-def getTableList():
-	con.row_factory = liteDb.Row
-	global cur
-	cur = con.cursor()
-	c = con.cursor()
-	cur.execute("Select name FROM sqlite_master Where type='table';")
-	rows = cur.fetchall()
-	tableList = PrettyTable(['Table(s)', 'No. Of Columns', 'Total Number of Rows'])
-	for row in rows:
-		cur.execute("Select * FROM "+row['name'])
-		fetchdata = cur.fetchall()
-		columnList = [description[0] for description in cur.description]
-
-		tableList.add_row([row['name'], len(columnList), len(fetchdata)])
-	print(tableList)
-
-def getQueries():
-	while True: 
-		try:
-			query = input("Query> :")
-			if query != "":
-				selectQuery(query)
-		except liteDb.OperationalError:
-			print('Oops invalid query Please try it again')
-
-def selectQuery(query):
-	global cur
-	try:
-		cur.execute(query)
-		columnList = [description[0] for description in cur.description]
-		queryResult = PrettyTable(columnList)
-		rows = cur.fetchall()
-		count = (len(rows))
-		rd = 0
-		result = []
-		while rd < len(columnList):
-			result.append('i['+str(rd)+']')
-			rd+=1
-		#print(result)
-		for i in rows:				
-			finalResult =  ','.join(result)
-			x = len(result) 
-			if x < 2:
-				queryResult.add_row([eval(finalResult)]) 
-			else:
-				queryResult.add_row(eval(finalResult))
-		print('Total Number of Row(s) Affected: ', count)
-		print(queryResult)
-	except TypeError:
-		osType = platform.system()
-		con.commit()
-		if osType != "Windows":
-			os.system("clear")
+	def __init__(self):
+		if (len(sys.argv) > 1):
+			SqliteAdmin.con = liteDb.connect(sys.argv[1])
 		else:
-			os.system("cls")
-		getTableList()
+			SqliteAdmin.con = liteDb.connect("example.db")
+		self.getTableList()
+		self.getQueries()	
 
-getTableList()
-getQueries()
+	def getTableList(self):
+		SqliteAdmin.con.row_factory = liteDb.Row
+		SqliteAdmin.cur = SqliteAdmin.con.cursor()
+		SqliteAdmin.cur.execute("Select name FROM sqlite_master Where type='table';")
+		rows = SqliteAdmin.cur.fetchall()
+		tableList = PrettyTable(['Table(s)', 'No. Of Columns', 'Total Number of Rows'])
+		for row in rows:
+			SqliteAdmin.cur.execute("Select * FROM "+row['name'])
+			fetchdata = SqliteAdmin.cur.fetchall()
+			columnList = [description[0] for description in SqliteAdmin.cur.description]
+
+			tableList.add_row([row['name'], len(columnList), len(fetchdata)])
+		print(tableList)
+
+	def getQueries(self):
+		while True: 
+			try:
+				query = input("Query> :")
+				if query != "":
+					self.selectQuery(query)
+			except liteDb.OperationalError:
+				print('Oops invalid query Please try it again')
+
+	def selectQuery(self,query):
+		global cur
+		try:
+			SqliteAdmin.cur.execute(query)
+			columnList = [description[0] for description in SqliteAdmin.cur.description]
+			queryResult = PrettyTable(columnList)
+			rows = SqliteAdmin.cur.fetchall()
+			count = (len(rows))
+			rd = 0
+			result = []
+			while rd < len(columnList):
+				result.append('i['+str(rd)+']')
+				rd+=1
+			#print(result)
+			for i in rows:				
+				finalResult =  ','.join(result)
+				x = len(result) 
+				if x < 2:
+					queryResult.add_row([eval(finalResult)])
+				else:
+					queryResult.add_row(eval(finalResult))
+			print('Total Number of Row(s) Affected: ', count)
+			print(queryResult)
+		except TypeError:
+			osType = platform.system()
+			SqliteAdmin.con.commit()
+			if osType != "Windows":
+				os.system("clear")
+			else:
+				os.system("cls")
+			self.getTableList()
+
+SqliteAdmin = SqliteAdmin()
